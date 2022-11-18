916fb7059075f9f94e9dfa8e74058d7adde4eb70
IsmaelSantiagoR <santiago@calimasolutions.com>
2022-11-18 16:00:13 +0000

    Robots txt

---
+++ app/Filament/Pages/RobotsTxt.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Forms;
+use Filament\Pages\Page;
+use Illuminate\Support\Facades\File;
+
+class RobotsTxt extends Page implements Forms\Contracts\HasForms
+{
+    use Forms\Concerns\InteractsWithForms;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static string $view = 'filament.pages.robots-txt';
+
+    public string $robots = '';
+
+    public function mount()
+    {
+        $this->form->fill([
+            'robots' => File::get(public_path('robots.txt'))
+        ]);
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            Forms\Components\Textarea::make('robots')->required(),
+        ];
+    }
+
+    public function save()
+    {
+        File::replace(public_path('robots.txt'), $this->robots);
+    }
+}
